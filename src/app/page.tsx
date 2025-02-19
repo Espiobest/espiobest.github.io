6006171d963c0b7436de46566365f4aa5744edf8
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-02-19 15:50:45 -0500

    remove hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Portfolio from './components/Portfolio';
 
 export default function Home() {
   return (
-
     <Portfolio />
-    // <div>
-      // <h1>Hello, world!</h1>
-    // </div>
   );
 }
