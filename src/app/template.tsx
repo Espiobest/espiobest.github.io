1915af018ebe976dde4aa06001112ffbba1f0d83
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-02-20 04:52:14 -0500

    reinstall framer motion

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,20 +4,18 @@ import { motion } from 'framer-motion';
 
 import { ReactNode } from 'react';
 
-interface TransitionProps {
-  children: ReactNode;
-}
 
-const Transition = ({ children }: TransitionProps) => {
-  return (
-    <motion.div
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.5 }}
-    >
-      {children}
-    </motion.div>
-  );
+const Transition = ({ children }: {children: ReactNode}) => {
+
+    return (
+        <motion.div
+            initial={{y: 20, opacity: 0 }}
+            animate={{y: 0, opacity: 1 }}
+            transition={{ease: 'easeInOut', duration: 0.5}}
+        >
+        {children}
+        </motion.div>
+    );
 };
 
 export default Transition;
