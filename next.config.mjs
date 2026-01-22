e7458050d748f72a83c3bc12fc76beacae71507b
Espiobest <59823894+Espiobest@users.noreply.github.com>
2026-01-22 20:02:00 +0530

    fix build issues (again)

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: isProd ? '/espiobest.github.io' : '',
+  basePath: '',
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
